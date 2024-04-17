ab8739c3e3be458813c4002c93b73d542ceebbba
Shaun Donnelly <shaundonnelly@gmail.com>
2024-04-17 10:23:50 +0100

    Update

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ The stuff I do when I get a new Mac.
 ## Getting started
 
 Download this repo then run `setup.sh`
+
+## Go further
+
+This repo does **not** do the following, which you probably want on a new computer:
+
+* Install [Xcodes](https://www.xcodes.app)
+* Install [NVM](https://github.com/nvm-sh/nvm). If you do install NVM, add the contents of `nvm-hook` in this repo to your `.zshrc` for convenience.
+* VSCode plugins
+
--- setup.sh
+++ setup.sh
@@ -9,14 +9,16 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 ### Install dev stuff
 
-# oh my zsh
+# oh my zsh, and plugins
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install zsh-syntax-highlighting
 
 # git
 brew install git
 
 # GitHub, plus login
 brew install gh
+gh auth login
 
 ### Apps
 brew install --cask iterm2
@@ -28,42 +30,6 @@ brew install --cask visual-studio-code
 mkdir projects
 
 ### Add custom functions to zshrc
-
-echo '
-
-###
-### START OF CUSTOM SCRIPTS
-###
-
-# Create a new directory and enter it
-function mkd() {
-	mkdir -p "$@" && cd "$_";
-}
-
-# Go to a project folder. If no args, goes to ~/projects
-# If an arg supplied, goes to ~/projects/<arg>
-function p {
-  cd ~/projects/$1
-}
-
-# Clear Swift package stuff
-# https://forums.developer.apple.com/forums/thread/673976
-function xcodeclear {
-  swift package purge-clean
-	swift package reset
-}
-
-alias bs="brew search"
-alias bi="brew install"
-alias bcu="brew uninstall"
-alias zedit="code ~/.zshrc"
-alias zload="source ~/.zshrc"
-alias yolo="git add -A && git commit --amend --no-edit && git push --force"
-
-###
-### END OF CUSTOM SCRIPTS
-###
-
-' >> ~/.zshrc
+cat ./zsh-things >> ~/.zshrc
 
 echo "Installed homebrew, git, github cli, oh my zsh, iterm and vscode, made the projects directory, and added custom aliases."
